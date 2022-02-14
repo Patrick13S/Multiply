2fef6a12cc1c396e392b7cf39fa6795e65df9583
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-14 14:20:19 -0700

    did not work

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var numberOne: UITextField!
+    
+    @IBOutlet weak var numberTwo: UITextField!
+    
+    @IBOutlet weak var result: UILabel!
+   
+    // if
+   // var result = (numberOne) * numberTwo
+    
+  
+                            
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func calculate(_ sender: Any)
+    {
+        var one = Int(numberOne.text)
+         
+       var two = Int(numberTwo.text)
+  var result = one * two)
+    }
 
+    
 }
 
